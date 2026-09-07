66fc6029e56c0bfee1f4ffab654292d4c66f4545
nalaaaa356-cloud <nalaaaa356@gmail.com>
2026-09-07 11:50:28 +0700

    Update python17.py

--- python17.py
+++ python17.py
@@ -1,5 +1,3 @@
-#program untuk melihat kalender
-#import packages calendar
 import calendar
 
 
@@ -7,5 +5,4 @@ yy = int(input("Masukkan tahun: "))
 mm = int(input("Masukkan bulan (dalam angka): "))
 
 
-# display the calendar
 print(calendar.month(yy, mm))
