946189ae88e13502e5ee9eb08a4e5fba4ee631be
nalaaaa356-cloud <nalaaaa356@gmail.com>
2026-09-07 11:51:28 +0700

    Update python19.py

--- python19.py
+++ python19.py
@@ -1,11 +1,9 @@
-#program untuk mencari angka yang dapat dibagi dengan angka lain
 n = int(input("Berapa jumlah angka dalam deret? "))
 min = int(input("Berapa angka terkecil? "))
 max = int(input("Berapa angka terbesar? "))
 divisible = int(input("Berapa angka yang ingin kamu bagi? "))
 
 
-#import packages random
 import random
 deret = []
 for i in range(0,n):
