6327d74bd1fae78a446533e9f4af19d810b8efc6
nalaaaa356-cloud <nalaaaa356@gmail.com>
2026-09-07 11:51:07 +0700

    Update python18.py

--- python18.py
+++ python18.py
@@ -1,6 +1,3 @@
-#program untuk menghitung jumlah huruf vokal pada sebuah kalimat
-#string of vowels
-
 import pandas as pd
 
 vowels = 'aeiou'
@@ -9,15 +6,12 @@ vowels = 'aeiou'
 ip_str = str(input("Masukkan sebuah kalimat: "))
 
 
-#memodifikasi kalimat agar memiliki format yang sama setiap karakter
 ip_str = ip_str.casefold()
 
 
-# membuat sebuah dictionary dengan setiap huruf vokal menjadi key dan value awal adalah 0
 count = {}.fromkeys(vowels,0)
 
 
-# menghitung jumlah huruf vokal
 for char in ip_str:
    if char in count:
        count[char] += 1
